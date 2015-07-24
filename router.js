9489829ac6e6d5545477b646f7ba5d191bd3bbb2
Ben Drucker <bvdrucker@gmail.com>
2015-07-23 18:39:41 -0700

    Init router

---
+++ router.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var Observ = require('observ')
+var location = require('global/document').location
+var history = require('./history')
+
+module.exports = Router
+
+function Router (data) {
+  var state = Observ(data.path || location ? location.pathname : '')
+  var inPopState = false
+
+  history.onPopState(onPopState)
+  state(onChange)
+
+  return state
+
+  function onPopState (path) {
+    inPopState = true
+    state.set(path)
+  }
+
+  function onChange (path) {
+    if (inPopState) {
+      inPopState = false
+      return
+    }
+    history.pushState(path)
+  }
+}
