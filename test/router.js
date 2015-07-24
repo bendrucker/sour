d589c4d0e6e9c8901b293ff36b50e18869e36503
Ben Drucker <bvdrucker@gmail.com>
2015-07-24 10:23:32 -0700

    Add tests against path handling via user set or pop state

--- test/router.js
+++ test/router.js
@@ -16,4 +16,39 @@ test('router (server)', function (t) {
   t.end()
 })
 
+test('router (browser) - set path', function (t) {
+  t.plan(2)
+  var Router = proxyquire('../router', {
+    'global/document': {
+      location: {pathname: '/the/initial/path'}
+    },
+    './history': {
+      onPopState: noop,
+      pushState: function (path) {
+        t.equal(path, '/the/set/path')
+      }
+    }
+  })
+  var router = Router()
+  t.equal(router(), '/the/initial/path')
+  router.set('/the/set/path')
+})
+
+test('router (browser) - pop state', function (t) {
+  var onPopState
+  var Router = proxyquire('../router', {
+    'global/document': {},
+    './history': {
+      onPopState: function (callback) {
+        onPopState = callback
+      },
+      pushState: t.fail.bind(t, 'pushState not called when pop is handled')
+    }
+  })
+  var router = Router()
+  onPopState('/the/popped/path')
+  t.equal(router(), '/the/popped/path')
+  t.end()
+})
+
 function noop () {}
