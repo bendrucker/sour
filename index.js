618fbee46986b9d4a0f4fb06045e0ed36c4cd021
Ben Drucker <bvdrucker@gmail.com>
2015-09-24 13:52:15 -0700

    Replace path observation and history with observ-path

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 'use strict'
 
-var Observ = require('observ')
-var location = require('global/document').location
-var history = require('./history')
+var Path = require('observ-path')
 var Matcher = require('./router')
 var View = require('./view')
 
 module.exports = Router
 
 function Router (path) {
-  var state = Observ(path || location ? location.pathname : '')
-  var inPopState = false
-
-  history.onPopState(onPopState)
-  state(onChange)
-
-  return state
-
-  function onPopState (path) {
-    inPopState = true
-    state.set(path)
-  }
-
-  function onChange (path) {
-    if (inPopState) {
-      inPopState = false
-      return
-    }
-    history.pushState(path)
-  }
+  return Path(path)
 }
 
 Router.render = function render (state, routes) {
