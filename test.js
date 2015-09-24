5179eddfc0d06940867b144e6da04cd3be95b801
Ben Drucker <bvdrucker@gmail.com>
2015-09-24 13:54:29 -0700

    Move tests to top level

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
 'use strict'
 
 var test = require('tape')
-var Router = require('../')
+var Sour = require('./')
+var Router = require('./router')
 
 test('integration', function (t) {
   t.plan(3)
-  var state = Router()
+  var state = Sour()
   var routes = {}
   function render (callback) {
-    callback(Router.render(state(), routes))
+    callback(Sour.render(state(), routes))
   }
   routes = {
     '/': function () {
@@ -36,4 +37,17 @@ test('integration', function (t) {
   render(function (content) {
     t.equal(content, 'Post 123')
   })
+})
+
+test('route matching', function (t) {
+  var match = Router({
+    '/page/:id': render
+  })
+  t.deepEqual(match('/page/123'), {
+    params: {id: '123'},
+    render: render
+  })
+  t.notOk(match('/page'))
+  t.end()
+  function render () {}
 })
