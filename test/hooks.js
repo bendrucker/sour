4311fa507a986deb2d26c59ec77a64f0bfb1835b
Ben Drucker <bvdrucker@gmail.com>
2015-12-05 21:43:19 -0800

    Add test for route hooks

--- test/hooks.js
+++ test/hooks.js
@@ -38,4 +38,44 @@ test('hooks', function (t) {
 
     Router.watch(state)
   })
+
+  t.test('route', function (t) {
+    t.plan(1)
+
+    var state = Router({
+      path: '/packages/sour'
+    })
+    var values = []
+
+    var pkg = Router.route(state, {
+      path: '/packages/:name',
+      render: function () {
+        return values.join(',')
+      }
+    })
+
+    var other = Router.route(state, {
+      path: '/other/route'
+    })
+
+    Router.hook(state, 'enter.before', function (params, callback) {
+      values.push(0, 1)
+      callback()
+    })
+
+    Router.hook(state, pkg, 'enter.before', function (params, callback) {
+      values.push(2, 3)
+      callback()
+    })
+
+    Router.hook(state, other, 'enter.before', function () {
+      t.fail('other route hook called')
+    })
+
+    state.active(function onChange () {
+      t.equal(Router.render(state()), '0,1,2,3')
+    })
+
+    Router.watch(state)
+  })
 })
