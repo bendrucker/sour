e44f930921c26f26687ad669ec53f5a59b78060a
Ben Drucker <bvdrucker@gmail.com>
2015-12-05 21:37:19 -0800

    Update hook test to test serial execution

--- test/hooks.js
+++ test/hooks.js
@@ -3,30 +3,39 @@
 var test = require('tape')
 var Router = require('../')
 
-test('global hook', function (t) {
-  t.plan(2)
+test('hooks', function (t) {
+  t.test('global', function (t) {
+    t.plan(2)
 
-  var state = Router({
-    path: '/packages/sour'
+    var state = Router({
+      path: '/packages/sour'
+    })
+    var values = []
+
+    Router.route(state, {
+      path: '/packages/:name',
+      render: function () {
+        return values.join(',')
+      }
+    })
+
+    Router.hook(state, 'enter.before', function (params, callback) {
+      t.deepEqual(params, {name: 'sour'})
+      process.nextTick(function () {
+        values.push(0)
+        callback()
+      })
+    })
+
+    Router.hook(state, 'enter.before', function (params, callback) {
+      values.push(1)
+      callback()
+    })
+
+    state.active(function onChange () {
+      t.equal(Router.render(state()), '0,1')
+    })
+
+    Router.watch(state)
   })
-  var value = null
-
-  Router.route(state, {
-    path: '/packages/:name',
-    render: function () {
-      return value
-    }
-  })
-
-  Router.hook(state, 'enter.before', function (params, callback) {
-    t.deepEqual(params, {name: 'sour'})
-    value = 'hooked'
-    callback()
-  })
-
-  state.active(function onChange () {
-    t.equal(Router.render(state()), 'hooked')
-  })
-
-  Router.watch(state)
 })
