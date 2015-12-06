0b1e80bd70ab9dd29458aee3cc9f4f04e1a7fd01
Ben Drucker <bvdrucker@gmail.com>
2015-12-05 21:33:22 -0800

    Nest rendering test

--- test/render.js
+++ test/render.js
@@ -3,21 +3,23 @@
 var test = require('tape')
 var Router = require('../')
 
-test('simple render', function (t) {
-  t.plan(1)
+test('render', function (t) {
+  t.test('simple', function (t) {
+    t.plan(1)
 
-  var state = Router()
+    var state = Router()
 
-  Router.route(state, {
-    path: '/',
-    render: function () {
-      return 'sour'
-    }
+    Router.route(state, {
+      path: '/',
+      render: function () {
+        return 'sour'
+      }
+    })
+
+    state.active(function onChange () {
+      t.equal(Router.render(state()), 'sour')
+    })
+
+    Router.watch(state)
   })
-
-  state.active(function onChange () {
-    t.equal(Router.render(state()), 'sour')
-  })
-
-  Router.watch(state)
 })
